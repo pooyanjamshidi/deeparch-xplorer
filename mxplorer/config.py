028a2fb22ed158db40627abfebd95591c8d332c1
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2017-12-12 01:09:31 -0500

    added code for running deep arch optimization experiment

---
+++ mxplorer/config.py
@@ -0,0 +1,41 @@
+import os, json
+
+import numpy as np
+
+DIR_PATH = os.path.dirname(__file__)
+
+PARAMETERS = [
+  dict(name='batch_size', type='int', bounds={'min': 8, 'max': 32}),
+  dict(name='conv_1_num_filters',  type='int', bounds={'min': 32, 'max': 256}),
+  dict(name='conv_1_filter_size', type='int', bounds={'min': 2, 'max': 10}),
+  dict(name='conv_2_num_filters', type='int', bounds={'min': 32, 'max': 256}),
+  dict(name='conv_2_filter_size', type='int', bounds={'min': 2, 'max': 10}),
+  dict(name='conv_3_num_filters', type='int', bounds={'min': 32, 'max': 256}),
+  dict(name='conv_3_filter_size', type='int', bounds={'min': 2, 'max': 10}),
+  dict(name='log_lr', type='double', bounds={'min': np.log(1e-10), 'max': np.log(1)}),
+  dict(name='log_beta_1', type='double', bounds={'min': np.log(1e-2), 'max': np.log(.5)}),
+  dict(name='log_beta_2', type='double', bounds={'min': np.log(1e-6), 'max': np.log(.5)}),
+  dict(name='log_epsilon', type='double', bounds={'min': np.log(1e-10), 'max': np.log(1e-6)}),
+  dict(name='log_decay', type='double', bounds={'min': np.log(1e-10), 'max': np.log(1e-1)}),
+]
+
+
+DATASET = 'Adiac'
+DATA_PATH = '../data'
+EXPERIMENT_PATH = '../exp'
+
+DATASET_FOLDER = os.path.join(DATA_PATH, 'UCR_TS_Archive_2015', DATASET)
+DATASET_FILE = DATASET
+DATASET_PATH = os.path.join(DIR_PATH, DATASET_FOLDER, DATASET_FILE)
+
+EXPERIMENT_NAME = 'exp'
+
+METRIC_1 = 'val_acc'
+METRIC_2 = 'inference_time'
+
+METRICS = [{'name': METRIC_1},
+           {'name': METRIC_2}]
+
+NB_EPOCHS = 500
+
+OBSERVATION_BUDGET = len(PARAMETERS) * 20 * 2
---
+++ mxplorer/run_exp.py
@@ -0,0 +1,83 @@
+from multiprocessing import Process, Queue
+import numpy as np
+from datetime import datetime
+import os
+import csv
+
+from mxplorer.config import (PARAMETERS, EXPERIMENT_NAME, METRICS, DATASET,
+                             OBSERVATION_BUDGET, DATASET_PATH, EXPERIMENT_PATH,
+                             METRIC_1, METRIC_2)
+
+from mxplorer.train_model import prepare_data, evaluate_assignments
+
+
+class Experiment:
+    def __init__(self, name, parameters, metrics, budget):
+        self.id = name + '_' + DATASET + '_' + str(datetime.now())
+        self.observation_count = 0
+        self.name = name
+        self.conf_space = parameters
+        self.metrics = metrics
+        self.budget = budget
+        self.ndim = len(self.conf_space)
+
+        csv_filename = self.id + '.csv'
+        myField = ["config_id", METRIC_1, METRIC_2]
+        with open(os.path.join(EXPERIMENT_PATH, csv_filename), 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=myField)
+            writer.writeheader()
+
+    class Configuration:
+        def __init__(self, parameters, budget):
+            self.id = 0
+            self.assignments = {}
+            self.conf_space = parameters
+            self.ndim = len(parameters)
+            self.configs = np.random.randint(2, size=(budget, self.ndim))
+
+        def get_assignment(self):
+            assignment = {}
+            config = self.configs[self.id, :]
+            for i in range(self.ndim):
+                option = self.conf_space[i]
+                if config[i] == 0:
+                    assignment[option["name"]] = option["bounds"]["min"]
+                else:
+                    assignment[option["name"]] = option["bounds"]["max"]
+            self.id += 1
+            self.assignments = assignment
+            return assignment
+
+    def add_observation(self, measurement):
+        self.observation_count += 1
+        csv_filename = self.id + '.csv'
+        myField = ["config_id", METRIC_1, METRIC_2]
+        with open(os.path.join(EXPERIMENT_PATH, csv_filename), 'a') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=myField)
+            writer.writerow(measurement)
+
+
+nb_classes, x_train, Y_train, x_test, Y_test = prepare_data(DATASET_PATH)
+
+q = Queue()
+
+exp = Experiment(name=EXPERIMENT_NAME,
+                 parameters=PARAMETERS,
+                 metrics=METRICS,
+                 budget=OBSERVATION_BUDGET)
+
+conf = exp.Configuration(PARAMETERS, OBSERVATION_BUDGET)
+
+while exp.observation_count < exp.budget:
+    conf.get_assignment()
+
+    p = Process(target=evaluate_assignments, args=(
+        q, exp, conf, x_train, Y_train,
+        x_test, Y_test, nb_classes)
+    )
+
+    p.start()
+    p.join()
+    data, metadata = q.get()
+
+    exp.add_observation(data)
